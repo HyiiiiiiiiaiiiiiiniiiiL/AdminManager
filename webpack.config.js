c03adcb02724028b8113229a32bfc7f37d8b2755
HiiYiiLii <892630853@qq.com>
2019-06-12 11:26:51 +0800

    webpack相关配置

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,15 @@
-var HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 
 module.exports = {
     entry: './src/app.jsx',
     output: {
         path: path.resolve(__dirname, 'dist'),//通过webpack打包的文件最终放到什么位置
-        filename: 'app.js'
+        publicPath: '/dist/',
+        filename: 'js/app.js'
     },
     module: {
         rules: [
@@ -21,14 +25,59 @@ module.exports = {
             },
             {
                 test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader']
+                })
+            },
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader', 'sass-loader']
+                })
+            },
+            {
+                test: /\.(png|jpg|gif)$/,
                 use: [
-                    'style-loader',
-                    'css-loader'
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 8192,
+                            name: 'resource/[name].[ext]'
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(eot|svg|ttf|woff|woff2|otf)$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 8192,
+                            name: 'resource/[name].[ext]'
+                        }
+                    }
                 ]
             }
         ]
     },
-    plugins: [new HtmlWebpackPlugin({
-        template: './src/index.html'
-    })]
+    plugins: [
+        //处理html文件
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        }),
+        //独立css文件
+        new ExtractTextPlugin("css/[name].css"),
+        //提出公共模块
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'common',
+            filename: 'js/base.js'
+        })
+    ],
+    devServer: {
+        port: 8086
+
+    }
 };
